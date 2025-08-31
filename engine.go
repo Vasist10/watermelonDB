19d5e762455194aef9d5654d9df0f5eff3186b7c
Vasist10 <155972527+Vasist10@users.noreply.github.com>
2025-08-31 17:50:16 +0530

    implemented page layout

--- engine.go
+++ engine.go
@@ -1,92 +1,201 @@
 package main
 
 import (
-	"bytes"
+	//"bytes"
 	"encoding/binary"
 	"os"
 	"io"
 	"errors"
 )
+const PageSize = 4096
+
+const (
+	numRecordOffset = 0
+	freeSpaceOffset = 2
+	pageHeaderSize = 4
+
+)
+
+type Page struct {
+	id 	int64
+	data [PageSize]byte
+	dirty bool //if pg is modified dirty is set to true and when we wb to disk we only weite the pages where this flag is set to 1
+}
+
+type RecordLocation struct {
+	PageID int64
+	PageOffset uint16
+}
+//index map string to RecordLocation:
+// Key                 Value (RecordLocation)
+// 
+// "name"       { PageID: 0, PageOffset: 4  }
+// "hello"    { PageID: 0, PageOffset: 30 }
+// "smthng"   { PageID: 1, PageOffset: 4  }
 
 type DB struct {
 	file *os.File
-	index map[string]int64
-}
+	index map[string]RecordLocation
+	totalPages int64
+	freelist []int64
+ }
+
 func Open(path string)(*DB,error){
 	f,err := os.OpenFile(path,os.O_RDWR|os.O_CREATE,0644)
 	if err != nil{
 		return nil, err
 	}
-	return &DB{file: f,
-		index : make(map[string]int64)}, nil
+	fileInfo, err := f.Stat()
+	if err != nil{
+		f.Close()
+		return nil, err
+	}
+	totalPages := fileInfo.Size() / PageSize
+	
+	return &DB{
+		file: f,
+		index : make(map[string]RecordLocation),
+		totalPages: totalPages,
+		freelist: make([]int64,0),
+		}, nil
 }
 
 func (db *DB) Close() error{
 	return db.file.Close()
 }
 
-func (db *DB) Put(key string, value string) error{
-	offset, err := db.file.Seek(0, io.SeekEnd)
+func (p *Page) numRecords() uint16{
+	return binary.LittleEndian.Uint16(p.data[numRecordOffset:])// littleendinan says to our comp that do not convert to text/eng keep it in binary itself i want to use in binary format only
+}
+
+func (p *Page) setNumRecords(n uint16){//write the records to the page header basically
+	binary.LittleEndian.PutUint16(p.data[numRecordOffset:], n)
+	p.dirty = true
+}
+func (p *Page) freeSpaceOffset() uint16{ // offset where the free space starts
+	return binary.LittleEndian.Uint16(p.data[freeSpaceOffset:])
+}
+func (p *Page) setFreeSpaceOffset(n uint16){//updating the frree space offset in the pg header
+	binary.LittleEndian.PutUint16(p.data[freeSpaceOffset:], n)
+	p.dirty = true
+}
+func (p *Page) addRecordtoPage(key string, value string) (uint16, error){
+	keyLen, valLen := uint32(len(key)), uint32(len(value))
+	recordSize := uint16(8+len(key)+len(value))
+	recordOffset := p.freeSpaceOffset()
+	amountFreeSpace := PageSize - recordOffset
+	currentRecords := p.numRecords()
+	if amountFreeSpace < recordSize {
+		return 0, errors.New("not enough space in page")
+	}
+
+	binary.LittleEndian.PutUint32(p.data[recordOffset:], keyLen)
+	binary.LittleEndian.PutUint32(p.data[recordOffset+4:], valLen)
+
+	
+	keyStart := recordOffset + 8
+	copy(p.data[keyStart:],key)
+	valStart := keyStart + uint16(len(key))
+	copy(p.data[valStart:], value)
+
+	p.setNumRecords(currentRecords + 1)
+	p.setFreeSpaceOffset(recordOffset + recordSize)
+
+	return recordOffset,nil 
+}
+func readPage(file *os.File, pageID int64)(*Page,error){
+	pg:= &Page{id:pageID}
+	_,err := file.Seek(pageID*PageSize, io.SeekStart)
+	if err != nil{
+		return nil,err
+	}
+	_,err = io.ReadFull(file,pg.data[:]);
+	if err !=nil{
+		return nil, err
+	}
+	return pg,nil
+
+}
+func writePage(file *os.File, pg *Page) error{
+	if !pg.dirty{
+		return nil
+	}
+	offset := pg.id * PageSize
+	
+	_,err := file.Seek(offset,io.SeekStart)
 	if err != nil{
 		return err
 	}
-	var b bytes.Buffer
-
-	KeyLen := int32(len(key))
-	ValLen := int32(len(value))
-	//&b → the writer (bytes.Buffer in our case).
-	//binary.LittleEndian → says we want to write the integer in little-endian byte order.
-	//Little-endian means the least significant byte goes first.
-	if err := binary.Write(&b,binary.LittleEndian,KeyLen); err != nil{
+	_,err = file.Write(pg.data[:])
+	if  err != nil{
 		return err
 	}
-	if err := binary.Write(&b,binary.LittleEndian,ValLen); err != nil{
-		return err
-	}
-	
-	b.Write([]byte(key))
-	b.Write([]byte(value))
-
-	if _,err := db.file.Write(b.Bytes()); err != nil{
-		return err
-	}
-
-	db.index[key] = offset
-
+	pg.dirty = false
 	return nil
 
+}
+func (db *DB) Put(key string, value string) error{
+	recordSize:= 8 + len(key) + len(value)
+	if recordSize > PageSize - pageHeaderSize{
+		return errors.New("record size exceeds page size")
+	}
+	var targetPage *Page 
+	pageID := int64(-1)
+	
+	if len(db.freelist) > 0{
+		pageID = db.freelist[len(db.freelist)-1]
+		db.freelist = db.freelist[:len(db.freelist)-1]
+		pg,err := readPage(db.file,pageID)
+		if err != nil{
+			return err
+		}
+		if PageSize - int(pg.freeSpaceOffset())>=recordSize{
+			targetPage = pg
+		}
+	}
+	if targetPage == nil{
+		pageID = db.totalPages
+		db.totalPages++
+		targetPage = &Page{id:pageID}
+		targetPage.setFreeSpaceOffset(pageHeaderSize)
+		targetPage.setNumRecords(0)
+	}
+	recordOffset, err := targetPage.addRecordtoPage(key,value)
+	if err != nil{
+		return err
+	}
+	db.index[key] = RecordLocation{
+		PageID: pageID,
+		PageOffset: recordOffset,
+	}
+	err = writePage(db.file,targetPage)
+	if err != nil{
+		return err
+	}
+	if PageSize - int(targetPage.freeSpaceOffset()) < 8 + len(key) + len(value){
+		db.freelist = append(db.freelist, pageID)
+	}
+	return nil
 }
 
 var ErrKeyNotFound = errors.New("key not found")
 
 func (db *DB) Get(key string)(string,error){
-	offset, ok := db.index[key]
+	loc, ok := db.index[key]
 	if !ok{
 		return "", ErrKeyNotFound
 	}
-	if _, err := db.file.Seek(offset, io.SeekStart); err != nil{
+	pg,err := readPage(db.file, loc.PageID)
+	if err != nil{
 		return "", err
 	}
-	var KeyLen int32
-	var ValLen int32
-	//4 bytes from the file, interprets them as an int32, and stores the result in our KeyLen variable.
-	if err := binary.Read(db.file,binary.LittleEndian,&KeyLen); err !=nil{ 
-		return "", err
-	}
-	//does same for valen 
-	if err := binary.Read(db.file,binary.LittleEndian,&ValLen); err !=nil{
-		return "", err
-	}
-	//skips the key bytes cz we already know the key
-	if _, err := db.file.Seek(int64(KeyLen), io.SeekCurrent); err != nil {
-        return "", err
-    }
-	//decalre a byte slice to store our val
-	valBytes := make([]byte, ValLen)
-	//reads the val bytes from the file 
-	if _, err := io.ReadFull(db.file,valBytes); err != nil{
-		return "", err
-	}
-	return string(valBytes), nil
+	recordOffset := loc.PageOffset
+	keyLen := binary.LittleEndian.Uint32(pg.data[recordOffset:])
+	valLen := binary.LittleEndian.Uint32(pg.data[recordOffset+4:])
 
+	valueStart := recordOffset + 8 + uint16(keyLen)
+	valueEnd := valueStart + uint16(valLen)
+	value := string(pg.data[valueStart:valueEnd])
+	return value, nil
+	
 }
--- main.go
+++ main.go
@@ -11,11 +11,18 @@ func main(){
 	if err != nil {
 		log.Fatalf("Failed to open database: %v", err)
 	}
+	
 
 	err = db.Put("name", "watermelonDB")
 	if err != nil {
 		log.Fatalf("Failed to put key: %v", err)
 	}
+	fileInfo, err := db.file.Stat()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("File size according to Go: %d bytes\n", fileInfo.Size())//gives me 4096 even though in my pc prop it shows 8kb
+
 	err = db.Put("hello", "namaste")
 	if err != nil {
 		log.Fatalf("Failed to put key: %v", err)
